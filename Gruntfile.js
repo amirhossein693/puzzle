f498d214935ebbabe2da0224b5c432ffefe52f9f
amirhossein <amirhossein693@gmail.com>
2014-11-18 12:48:43 +0330

    adding copy module and task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      // define the files to lint
+      files: ['gruntfile.js', 'app/assets/js/*.js'],
+      // configure JSHint (documented at http://www.jshint.com/docs/)
+      options: {
+        // more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+
     uglify: {
       build: {
         files: [{
@@ -27,19 +42,17 @@ module.exports = function(grunt) {
       }
     },
 
-    jshint: {
-      // define the files to lint
-      files: ['gruntfile.js', 'app/assets/js/*.js'],
-      // configure JSHint (documented at http://www.jshint.com/docs/)
-      options: {
-        // more options here if you want to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true
-        }
-      }
-    }
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'app/',
+          src: ['**', '!assets/js', '!assets/css'],
+          dest: 'dest/'
+        }],
+      },
+    },    
 
   });
 
@@ -47,9 +60,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // task(s).
-  grunt.registerTask('build', [ 'jshint', 'uglify:build', 'cssmin:build']);
+  grunt.registerTask('build', [ 'jshint', 'uglify:build', 'cssmin:build', 'copy:build']);
   grunt.registerTask('serve', ['jshint']);  
 
 };
